e5f094c923359372e4580fd93fea015a54ec60a0
leonardomarrao <61155148+leonardomarrao@users.noreply.github.com>
2023-08-30 02:44:53 +0100

    simplificaLocalStorage

--- frontend/pnafg-hub/src/pages/Login.jsx
+++ frontend/pnafg-hub/src/pages/Login.jsx
@@ -2,20 +2,26 @@ import React from 'react'
 import LoginForm from '../components/LoginForm'
 import Axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 const Login = () => {
   let navigate = useNavigate();
+
+  const user = JSON.parse(localStorage.getItem("user"));
+  
+  useEffect(() => {
+    if(user.user != null && user.refresh_token != null && user.access_token != null) {
+        navigate('/');
+    }
+  },[])
+  
   const loginFunc = (username, password) => {
     Axios.post("http://localhost:3000/api/auth/login", {
       username: username,
       password: password
     })
     .then((res) => {
-      //console.log(res.data)
-      localStorage.setItem('user', JSON.stringify(res.data.user))
-      localStorage.setItem('refresh_token', JSON.stringify(res.data.refresh_token))
-      localStorage.setItem('access_token', JSON.stringify(res.data.access_token))
-      console.log(JSON.parse(localStorage.getItem("user")))
+      localStorage.setItem('user', JSON.stringify(res.data))
       navigate("/")
     })
     .catch((err) => {
